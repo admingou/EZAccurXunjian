7b03180f45acb3be21e4e34ef86079dbf734d91c
admingou <1848065398@qq.com>
2023-01-17 14:28:26 +0800

    全局异常处理结构

--- api/api_v1/views.py
+++ api/api_v1/views.py
@@ -1,7 +1,9 @@
 from flask import request
 from . import api
+from EZAccur_xunjian_server.tools.error.codes import Success, Error
 
 
+@api.route("/test", methods=["GET"])
 def  index_api():
     if request.method == "GET":
-        return
+        return Success(msg="测试数据", data={"name":"gousl","age":24,"project": "go开发工程师"})
--- app.py
+++ app.py
@@ -1,12 +1,42 @@
 from flask import Flask
 from flask_cors import *
 from EZAccur_xunjian_server.api.api_v1 import api
+from EZAccur_xunjian_server.tools.error.isweb import is_web_or_api
+from EZAccur_xunjian_server.tools.error.web_error import web_errors
+from EZAccur_xunjian_server.tools.error import APIException
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-app.register_blueprint(api)
+app.register_blueprint(api, url_prefix="/api/v1")
 # app.config.from_object(config)
+
+@app.errorhandler(Exception)
+def  framework_error(e):
+    """
+    拦截未知异常问题
+    异常种类:APIException,
+    HTTPException,
+    Exception
+    :param e:
+    :return:
+    """
+    if is_web_or_api():
+        if    isinstance(e,APIException):
+              return  e
+        if    isinstance(e,HTTPException):
+            code = e.code
+            msg = e.description
+            status_code = 404
+            return  APIException(status_code,msg,code)
+        else:
+            if  not  app.config["DEBUG"]:
+                return APIException()
+            else:
+                raise e
+    else:
+        return web_errors()
 
 
 
